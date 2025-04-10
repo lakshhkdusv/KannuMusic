7e97557af649286fd7c72a565ce07f8f409d0fb4
lakshhkdusv <cookiesnew0@gmail.com>
2025-04-10 08:41:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MUNNA_PROOF")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+efdLDNVA719iN2E9")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+0qG_bXL8vhk4M2Jl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
